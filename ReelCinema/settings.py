48215271004c81daa945e7f0ccd0b6aeb76dd0ab
MagMc007 <mergashasho7@gmail.com>
2025-08-23 14:25:34 +0300

    project configed again + made migrations

--- ReelCinema/settings.py
+++ ReelCinema/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# change the model for user
+AUTH_USER_MODEL = "users.User"
 
 # Application definition
 
--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,7 @@
-from .views import SignUpView, SingInView, dummy, LogoutView, userChangeView
 from django.urls import path
 
 app_name = "users"
 
 urlpatterns = [
-    path("register/", SignUpView.as_view(), name="register-view"),
-    path("login/", SingInView.as_view(), name="login-view"),
-    path("dummy/", dummy, name="dummy"),
-    path("logout/", LogoutView.as_view(), name="logout-view"),
-    path("change/", userChangeView, name="change-view"),
+    
 ]
