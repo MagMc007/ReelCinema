92129bc923680a60e35e847b288c3ee4fb37817f
MagMc007 <mergashasho7@gmail.com>
2025-08-24 22:36:23 +0300

    fixed version of view + url

--- movies/views.py
+++ movies/views.py
@@ -10,9 +10,9 @@ OMDB_KEY = "58c421c3"
 class MovieView(APIView):
     """ provides view for movies by title"""
     def get(self, request, title):
-        # fetch movie from database using title
+        # fetch the first movie from database using title
 
-        movie = Movie.objects.filter(title=title)
+        movie = Movie.objects.filter(title__iexact=title).first()
         if movie:
             # serialize the movie data
 
@@ -22,22 +22,26 @@ class MovieView(APIView):
             )
         
         # if not in db fetch from OMDB
-        url = f"http://www.omdbapi.com/?apikey=[{OMDB_KEY}]&s={title}"
+        url = f"http://www.omdbapi.com/?apikey={OMDB_KEY}&s={title}"
         r = requests.get(url)
         data = r.json()
 
         # check the OMDB response
-        if data.Response:
+        if data.get("Response") == "True":
+            # separate movie from rest of json response
+            search_value = data["Search"][0]
+
             # create the movie in db first
-            Movie.objects.create(
-                "title": data["Title"],
-                "year": data["Year"],
-                "imdb_id": data["imdbID"],
-                "type": data["Type"],
-                "poster": data["Poster"]
+            movie = Movie.objects.create(
+                title=search_value["Title"],
+                year=search_value["Year"],
+                imdb_id=search_value["imdbID"],
+                type=search_value["Type"],
+                poster=search_value["Poster"]
             )
+            serialized_movie = MovieSerializer(movie)
             return Response(
-                MovieSerializer(data)
+                serialized_movie.data
             )
 
         return Response({"error": "Movie not found"}, status=404)
